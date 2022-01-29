7f1d953d5f71606fbb192e57cfc802310e3b3f18
Ankita Das <73665584+ankita-das-26@users.noreply.github.com>
2022-01-29 13:20:15 +0530

    Add files via upload

---
+++ inc/header.php
@@ -0,0 +1,21 @@
+
+
+
+<header class="header">
+  <div class="main-cont">
+    <div class="text">
+      <h2>Connect With the World Now!!</h2>
+      
+      <br>
+      <br>
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+      <a href="signup.php" class="link">
+        <button class="act-btn">
+          Chat-Now
+        </button>
+      </a>
+    </div>
+    <div class="img-cont"><img src="https://i.pinimg.com/originals/3a/8a/6c/3a8a6cc532e966db07c76f0041fe7a7b.png" 
+                           style="width:450px;height:450px;"></div>
+  </div>
+</header>
---
+++ inc/nav.php
@@ -0,0 +1,19 @@
+  <!-- navbar -->
+  <nav class="navbar">
+    <a href="index.php" class="navbar-brand">
+      Lets-Chat
+    </a>
+    <!-- Links -->
+    <link href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap" rel="stylesheet">
+    <ul class="list">
+      <li class="nav-item">
+        <a class="nav-link" href="login.php" class="login">Login</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="signup.php" class="signup">Signup</a>
+      </li>
+    </ul>
+  </nav>
+
+</body>
+</html>
